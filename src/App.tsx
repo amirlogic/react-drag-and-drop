3e96685f29f8ea56aa33d26fdded06711eac39fd
Amir Hachaichi <amir.hac@outlook.com>
2022-05-14 01:08:03 +0200

    DND animation works

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,77 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+
+
 
 function App() {
+
+  const onBeforeCapture = () => { // useCallback(, []);
+    console.log('Before Capture');
+  }
+  const onBeforeDragStart = () => { // useCallback(, []);
+    console.log('Before Drag start');
+  }
+  const onDragStart = () => { // useCallback(, []);
+    console.log('Drag started');
+  }
+  const onDragUpdate = () => { // useCallback(, []);
+    console.log('Drag update');
+  }
+  const onDragEnd = () => { // useCallback(, []);
+    console.log('Drag End');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <DragDropContext
+      onBeforeCapture={onBeforeCapture}
+      onBeforeDragStart={onBeforeDragStart}
+      onDragStart={onDragStart}
+      onDragUpdate={onDragUpdate}
+      onDragEnd={onDragEnd}
+    >
+      
+          <Droppable droppableId="droppable-1">
+          {(provided, snapshot) => (
+            
+          <div ref={provided.innerRef} className="container mx-auto px-4 py-1.5 space-y-2 my-8 bg-zinc-100 rounded-md shadow" {...provided.droppableProps}>
+
+            <Draggable draggableId="draggable-1" index={0}>
+            {(provided, snapshot) => (
+
+              <div ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps} className="p-6 rounded-sm shadow-sm cursor-move mb-2 bg-zinc-50">Item 1</div>
+            )}
+            </Draggable>
+
+            <Draggable draggableId="draggable-2" index={1}>
+            {(provided, snapshot) => (
+
+              <div ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps} className="p-6 rounded-sm shadow-sm cursor-move mb-2 bg-zinc-50">Item 2</div>
+            )}
+            </Draggable>
+
+            <Draggable draggableId="draggable-3" index={2}>
+            {(provided, snapshot) => (
+
+              <div ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps} className="p-6 rounded-sm shadow-sm cursor-move mb-2 bg-zinc-50">Item 3</div>
+            )}
+            </Draggable>
+          
+          {provided.placeholder}
+          </div>
+
+          )}
+            
+          </Droppable>
+
+    </DragDropContext>
   );
 }
 
