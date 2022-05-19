a91d34d29f11b1ba21cf485a35926c74d4d2e846
Amir Hachaichi <amir.hac@outlook.com>
2022-05-19 17:45:21 +0200

    push to GH

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 1. create rect app wth template typescript
 
-2. Install and configure tailwind css
+2. Install and configure tailwind css (requires react-create-app v5+)
 
 3. Install react-beautiful-dnd (using --force due to dep problems with react 18)
 
@@ -10,9 +10,14 @@
 https://github.com/atlassian/react-beautiful-dnd/blob/master/docs/api/drag-drop-context.md
 
 5. Wrap with Droppable, add children function
-
+https://github.com/atlassian/react-beautiful-dnd/blob/master/docs/api/droppable.md
 
 6. Add Draggables, with their children functions
+https://github.com/atlassian/react-beautiful-dnd/blob/master/docs/api/draggable.md
 
 7. Minimum requirements for the drag to appear
 (Does not work with React Strict Mode Wrappers)
+
+8. Setting up responders
+https://github.com/atlassian/react-beautiful-dnd/blob/master/docs/guides/responders.md
+
--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,22 @@ import React from 'react';
 
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
-
-
 function App() {
-
   const onBeforeCapture = () => { // useCallback(, []);
     console.log('Before Capture');
-  }
+  };
   const onBeforeDragStart = () => { // useCallback(, []);
     console.log('Before Drag start');
-  }
+  };
   const onDragStart = () => { // useCallback(, []);
     console.log('Drag started');
-  }
+  };
   const onDragUpdate = () => { // useCallback(, []);
     console.log('Drag update');
-  }
+  };
   const onDragEnd = () => { // useCallback(, []);
     console.log('Drag End');
-  }
+  };
 
   return (
 
@@ -31,45 +28,63 @@ function App() {
       onDragUpdate={onDragUpdate}
       onDragEnd={onDragEnd}
     >
-      
-          <Droppable droppableId="droppable-1">
-          {(provided, snapshot) => (
-            
+
+      <Droppable droppableId="droppable-1">
+        {(provided, snapshot) => (
+
           <div ref={provided.innerRef} className="container mx-auto px-4 py-1.5 space-y-2 my-8 bg-zinc-100 rounded-md shadow" {...provided.droppableProps}>
 
             <Draggable draggableId="draggable-1" index={0}>
-            {(provided, snapshot) => (
+              {(provided, snapshot) => (
 
-              <div ref={provided.innerRef}
-                    {...provided.draggableProps}
-                    {...provided.dragHandleProps} className="p-6 rounded-sm shadow-sm cursor-move mb-2 bg-zinc-50">Item 1</div>
-            )}
+                <div
+                  ref={provided.innerRef}
+                  {...provided.draggableProps}
+                  {...provided.dragHandleProps}
+                  className="p-6 rounded-sm shadow-sm cursor-move mb-2 bg-zinc-50"
+                >
+                  Item 1
+
+                </div>
+              )}
             </Draggable>
 
             <Draggable draggableId="draggable-2" index={1}>
-            {(provided, snapshot) => (
+              {(provided, snapshot) => (
 
-              <div ref={provided.innerRef}
-                    {...provided.draggableProps}
-                    {...provided.dragHandleProps} className="p-6 rounded-sm shadow-sm cursor-move mb-2 bg-zinc-50">Item 2</div>
-            )}
+                <div
+                  ref={provided.innerRef}
+                  {...provided.draggableProps}
+                  {...provided.dragHandleProps}
+                  className="p-6 rounded-sm shadow-sm cursor-move mb-2 bg-zinc-50"
+                >
+                  Item 2
+
+                </div>
+              )}
             </Draggable>
 
             <Draggable draggableId="draggable-3" index={2}>
-            {(provided, snapshot) => (
+              {(provided, snapshot) => (
 
-              <div ref={provided.innerRef}
-                    {...provided.draggableProps}
-                    {...provided.dragHandleProps} className="p-6 rounded-sm shadow-sm cursor-move mb-2 bg-zinc-50">Item 3</div>
-            )}
+                <div
+                  ref={provided.innerRef}
+                  {...provided.draggableProps}
+                  {...provided.dragHandleProps}
+                  className="p-6 rounded-sm shadow-sm cursor-move mb-2 bg-zinc-50"
+                >
+                  Item 3
+
+                </div>
+              )}
             </Draggable>
-          
-          {provided.placeholder}
+
+            {provided.placeholder}
           </div>
 
-          )}
-            
-          </Droppable>
+        )}
+
+      </Droppable>
 
     </DragDropContext>
   );
